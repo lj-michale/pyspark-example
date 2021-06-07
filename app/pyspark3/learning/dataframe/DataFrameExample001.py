560db4035d519877ecaf1f47a28d1da30595ab3a
ircloud <realtimecompute.luojie@gmail.com>
2021-06-07 17:06:04 +0800

    feat: dataframe by pyspark3

---
+++ app/pyspark3/learning/dataframe/DataFrameExample001.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-#
+
+# -------------------------------------------------------------------------------
+# Name:         DataFrameExample001
+# Description:
+# Author:       orange
+# Date:         2021/6/7
+# -------------------------------------------------------------------------------
+
+from pyspark.sql import SparkSession
+
+from common.log.logger import Log4j
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .master("local[3]") \
+        .appName("HelloSparkSQL") \
+        .getOrCreate()
+
+    logger = Log4j(spark)
+
+    surveyDF = spark.read \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv("file:///E:\OpenSource\GitHub\pyspark-example\dataset\sample.csv")
+
+    surveyDF.createOrReplaceTempView("survey_tbl")
+    countDF = spark.sql("select Country, count(1) as count from survey_tbl where Age<40 group by Country")
+
+    countDF.show()
