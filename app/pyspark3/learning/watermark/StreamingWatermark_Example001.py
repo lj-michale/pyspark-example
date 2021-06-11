22fa5ea3f50bfa78cc26a343053e15b02b8d5e4e
ircloud <realtimecompute.luojie@gmail.com>
2021-06-11 14:09:15 +0800

    feat: watermark

---
+++ app/pyspark3/learning/watermark/StreamingWatermark_Example001.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-#
+
+# -------------------------------------------------------------------------------
+# Name:         StreamingWatermark_Example001
+# Description:
+# Author:       orange
+# Date:         2021/6/11
+# -------------------------------------------------------------------------------
+
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import from_json, col, to_timestamp, window, expr, sum
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+
+from common.log.logger import Log4j
+
+
+if __name__ == "__main__":
+
+    spark = SparkSession \
+        .builder \
+        .appName("File Streaming Demo") \
+        .master("local[3]") \
+        .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .config("spark.sql.streaming.schemaInference", "true") \
+        .getOrCreate()
+
+    logger = Log4j(spark)
+
+    impressionSchema = StructType([
+        StructField("ImpressionID", StringType()),
+        StructField("CreatedTime", StringType()),
+        StructField("Campaigner", StringType())
+    ])
+
+    clickSchema = StructType([
+        StructField("ImpressionID", StringType()),
+        StructField("CreatedTime", StringType())
+    ])
+
+    kafka_impression_df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "impressions") \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    impressions_df = kafka_impression_df \
+        .select(from_json(col("value").cast("string"), impressionSchema).alias("value")) \
+        .selectExpr("value.ImpressionID", "value.CreatedTime", "value.Campaigner") \
+        .withColumn("ImpressionTime", to_timestamp(col("CreatedTime"), "yyyy-MM-dd HH:mm:ss")) \
+        .drop("CreatedTime") \
+        .withWatermark("ImpressionTime", "30 minute")
+
+    kafka_click_df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "clicks") \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    clicks_df = kafka_click_df.select(
+        from_json(col("value").cast("string"), clickSchema).alias("value")) \
+        .selectExpr("value.ImpressionID as ClickID", "value.CreatedTime") \
+        .withColumn("ClickTime", to_timestamp(col("CreatedTime"), "yyyy-MM-dd HH:mm:ss")) \
+        .drop("CreatedTime") \
+        .withWatermark("ClickTime", "30 minute")
+
+    join_expr = "ImpressionID == ClickID"
+    join_type = "inner"
+
+    joined_df = impressions_df.join(clicks_df, expr(join_expr), join_type)
+
+    output_query = joined_df.writeStream \
+        .format("console") \
+        .outputMode("append") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .trigger(processingTime="1 minute") \
+        .start()
+
+    logger.info("Waiting for Query")
+    output_query.awaitTermination()
